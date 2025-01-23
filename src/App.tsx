26ba73e68c1a7e99fee2e60a3f1a2f4e0bd6b91c
JinJu <juehdwn@naver.com>
2025-01-23 12:40:59 +0900

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
+  const { user, signOut } = useAuthenticator(); // 로그인 정보를 가져오고 로그아웃 기능 추가
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -14,12 +16,23 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content });
+    }
   }
 
   return (
     <main>
       <h1>My todos</h1>
+      {user ? ( // 사용자가 로그인했을 경우 표시
+        <div>
+          <p>Welcome, {user.username}!</p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      ) : (
+        <p>Please log in to manage your todos.</p>
+      )}
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
